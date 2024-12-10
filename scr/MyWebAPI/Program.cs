c1013b96ab782652cd220590ccf58624328a22bc
Michael Koch <m.koch@neuko-devment.com>
2024-12-10 23:28:40 +0100

    Add Docker support and update project configurations
    Updated the Visual Studio Solution file to include new project references and configurations. Modified `MyWebAPI.csproj` and `WebFrontEnd.csproj` to include Docker settings and new package references. Updated `MyWebAPI.http` to change the host address. Enhanced `Program.cs` to include Redis caching services and use controllers. Updated `launchSettings.json` for both projects to change application URLs and add Docker container settings. Cleaned up `Error.cshtml.cs` and updated `Index.cshtml` and `Index.cshtml.cs` in `WebFrontEnd` to display messages and make HTTP calls to `MyWebAPI`. Added new `.dockerignore`, `docker-compose.dcproj`, `docker-compose.override.yml`, `docker-compose.yml`, and `launchSettings.json` files for Docker Compose settings. Added new `Dockerfile` for `MyWebAPI` and `WebFrontEnd`. Added `WeatherForecast.cs` and `CounterController.cs` to `MyWebAPI` for new API functionalities.

--- scr/MyWebAPI/Program.cs
+++ scr/MyWebAPI/Program.cs
@@ -1,32 +1,24 @@
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "redis:6379"; // redis is the container name of the redis service. 6379 is the default port
+    options.InstanceName = "SampleInstance";
+});
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
